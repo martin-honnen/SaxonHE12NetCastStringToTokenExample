71f77bfa8778a1e7619c0fd4a9db085045ce4a29
Martin Honnen <martin.honnen@gmx.de>
2024-07-05 18:36:53 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using net.liberty_development.SaxonHE12s9apiExtensions;
+using net.sf.saxon.s9api;
+
+var processor = new Processor();
+
+var docBuilder = processor.newDocumentBuilder();
+
+var xmlDoc = docBuilder.Build(new FileInfo("sample1.xml"));
+
+var xpathProcessor = processor.newXPathCompiler();
+
+var items = xpathProcessor.evaluate("//item/token", xmlDoc);
+
+for (var i = 0; i < items.size(); i++)
+{
+    var item = items.itemAt(i);
+    Console.WriteLine($"string value: |{item.getStringValue()}|; cast as xs:token : |{xpathProcessor.evaluateSingle(". cast as xs:token", item)}|");
+}
